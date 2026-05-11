c6e66941dd1d75f00007cc5feb48b00214244519
jakepronger <73009116+jakepronger@users.noreply.github.com>
2026-05-11 13:06:00 +1000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Total Minimalist Transformation
+
+# 1. Kill the background daemons
+systemctl stop dnf-makecache.timer firewalld kdump tuned rpcbind oracle-cloud-agent
+systemctl disable dnf-makecache.timer firewalld kdump tuned rpcbind oracle-cloud-agent
+
+# 2. Clean the package manager
+dnf clean all
+rm -rf /var/cache/dnf
+
+dnf install -y dnf-utils
+dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+systemctl enable --now docker
+docker swarm init --advertise-addr 10.0.0.200
+
+curl -sSL https://raw.githubusercontent.com/jakepronger/dokploy/refs/heads/main/install.sh | sh
